df266149be5fe0133bba0323f549fa8fe0fe5edc
marcos-renato <marcos.vicente@mrpv.org>
2023-06-29 12:53:34 -0300

    Adicionando exemplo da calculadora

---
+++ calc.js
@@ -0,0 +1,10 @@
+
+var SomaFunc = require("./src/somar");
+var divisaoFunc = require("./src/divisao");
+var multiFunc = require("./src/multiplicacao");
+var subtracaoFunc = require("./src/subtracao");
+
+console.log(SomaFunc(6,3));
+console.log(divisaoFunc(6,3));
+console.log(multiFunc(6,3));
+console.log(subtracaoFunc(6,3));
