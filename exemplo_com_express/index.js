54a24c2be174f57f558ba83791e3b67cc4d3aec6
Marcos Renato Pinto Vicente <marcos.vicente@mrpv.org>
2023-07-02 22:17:47 -0300

    exemplo node com framework express e rotas

---
+++ exemplo_com_express/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("Seja bem vindo ao meu app!");
+})
+app.get("/sobre",function(req,res){
+    res.send("Minha pagina sobre!");
+})
+app.get("/blog",function(req,res){
+    res.send("Seja bem vindo ao meu blog!");
+})
+
+app.listen(8081,function(){
+    console.log('servidor rodando em http://localhost:8081/');
+})
